113417cafae1606ca2b108daae3fa0b0a5c62178
Ranbir Paul <ranbir.paul@in.abb.com>
2019-04-30 14:43:11 +0530

    Latest code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
 import {CoreModule} from './core/core.module';
 import {TenantModule} from './feature/tenant/tenant.module';
 import {DatacenterModule} from './feature/datacenter/datacenter.module';
 import {EquipmentModule} from './feature/equipment/equipment.module';
 import {RackModule} from './feature/rack/rack.module';
 import {UserModule} from './feature/user/user.module'
-
 import {ObjectModule} from './feature/object/object.module';
 import {ConfigurationModule} from './feature/configuration/configuration.module';
 import {SharedModule} from './shared/shared.module';
 import {MaterialModule} from './material';
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { AuthenticationGuard, MsAdalAngular6Module } from 'microsoft-adal-angular6';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { AuthenticationService } from './authentication.service';
-import { WelcomeComponent } from './welcome/welcome.component';
+import {AuthenticationGuard, MsAdalAngular6Module } from 'microsoft-adal-angular6';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {AuthenticationService} from './authentication.service';
+import {WelcomeComponent} from './welcome/welcome.component';
 import {ObjectTypeConfigurationAddComponent} from './feature/configuration/component/object-type-configuration-add/object-type-configuration-add.component';
 import {ObjectTypeAddComponent} from './feature/configuration/component/object-type-add/object-type-add.component';
 import {ObjectTypeListComponent} from './feature/configuration/component/object-type-list/object-type-list.component'
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import {EdgeModule} from './feature/edge/edge.module';
 @NgModule({
   declarations: [
@@ -38,10 +35,12 @@ import {EdgeModule} from './feature/edge/edge.module';
     AppRoutingModule,
     MsAdalAngular6Module.forRoot({
       tenant: '372ee9e0-9ce0-4033-a64a-c07073a91ecd',
-      clientId: '5a7fbb94-305d-485a-a4bb-027780060d64',
+      clientId: 'e9906b20-2ced-4bf9-9efd-6256b42d00bb', 
       redirectUri: window.location.origin,
+      authority: 'https://login.microsoftonline.com/372ee9e0-9ce0-4033-a64a-c07073a91ecd/oauth2/authorize',
       endpoints: {
-        "https://localhost/Api/": "xxx-bae6-4760-b434-xxx"
+        'https://ABB.onmicrosoft.com/APIApplication': 'bff75af7-b065-425c-a2fe-2117713debb9',
+        'https://graph.microsoft.com': 'https://graph.microsoft.com'
       },
       navigateToLoginRequestUrl: false,
       cacheLocation: 'localStorage',
@@ -59,7 +58,7 @@ import {EdgeModule} from './feature/edge/edge.module';
     ConfigurationModule,
     EdgeModule
   ],
-  providers: [AuthenticationGuard,AuthenticationService],
+  providers: [AuthenticationGuard, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,24 +1,55 @@
 import { Injectable } from '@angular/core';
 import { MsAdalAngular6Service } from 'microsoft-adal-angular6';
+import { HttpClient } from '@angular/common/http';  
+import { HttpHeaders } from '@angular/common/http';  
+import { HttpResponse } from '@angular/common/http';  
+import { Observable } from 'rxjs';
+import { httpFactory } from '@angular/http/src/http_module';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
+export class AuthenticationService 
+{
+  public isAuthor:boolean=true;
+  constructor(private adalSvc: MsAdalAngular6Service, private httpclient: HttpClient) 
+  {
+  }
+   
+   logOut()
+   {
+     this.adalSvc.logout();
+   }
 
-isAuthor:boolean=true;
-  constructor(private adalSvc: MsAdalAngular6Service) {
+   loginInfo()
+   {
+      this.isAuthor=true;
+      return this.adalSvc.LoggedInUserName;
+   }
+
+   ngOnInit() 
+   {
+   }
+
+   getAccessToken()
+   {
+      var accessToken:string;
+      if (this.adalSvc.isAuthenticated)
+      {
+        // Will take care of id_token, access_token and refresh_token
+        var tokenresult = this.adalSvc.acquireToken('https://ABB.onmicrosoft.com/APIApplication');
+        tokenresult.subscribe(token => {
+        // Get the access token here
+        accessToken = token;
+        console.log(accessToken);
+      },
+      err => {
+              console.log(err);
+        }
+      );
+      }
     
+      return accessToken;
    }
-   logOut(){
-    this.adalSvc.logout();
-   }
-
-   loginInfo(){
-    this.isAuthor=true;
-     /*if(!this.adalSvc.LoggedInUserName.includes('Guru')){
-       this.isAuthor=false;
-     }*/
-     return this.adalSvc.LoggedInUserName;
-   }
-}
+  }
